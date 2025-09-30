88f1f4e68497633df19fb5b90fd824ee1f15c1d5
Anrey <brisoanrey@gmail.com>
2025-09-30 16:03:57 +0800

    Create test_main.py

---
+++ app/test_main.py
@@ -0,0 +1,12 @@
+python
+import unittest
+from app.main import add
+class TestApp(unittest.TestCase):
+def test_add(self):
+self. assertEqual(add (2, 3), 5)
+
+if
+• name
+= " main_
+*:
+unittest.main()
